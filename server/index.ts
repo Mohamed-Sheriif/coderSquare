a3e51eeb4b6d2258f4e77434cf2606c4a4df736f
MohamedSherif <mohamedsh2782@gmail.com>
2022-12-13 11:03:56 +0200

    api type checking in types.ts, post handlers

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,6 @@
 import express, { RequestHandler } from "express";
 import { db } from "./datastore/index";
+import { createPostHandler, listPostHandler } from "./handlers/postHandler";
 
 const app = express();
 
@@ -14,19 +15,13 @@ const requestLoggerMiddleware: RequestHandler = (req, res, next) => {
 
 const posts: any[] = [];
 
-//middleware
+// Middleware
 app.use(express.json());
 app.use(requestLoggerMiddleware);
 
-app.get("/posts", (req, res) => {
-  res.json({ posts: db.listPosts() });
-});
-
-app.post("/posts", (req, res) => {
-  const post = req.body;
-  db.createPost(post);
-  res.sendStatus(200);
-});
+// Routes
+app.get("/posts", listPostHandler);
+app.post("/posts", createPostHandler);
 
 const PORT = 3000;
 app.listen(PORT, () => {
