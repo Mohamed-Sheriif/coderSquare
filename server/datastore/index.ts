75071a6576fd9d59953e3eff9ef860724949d14a
MohamedSherif <mohamedsh2782@gmail.com>
2022-12-14 11:28:26 +0200

    wip , added sqllite

--- server/datastore/index.ts
+++ server/datastore/index.ts
@@ -2,7 +2,13 @@ import { UserDao } from "./dao/UserDao";
 import { CommentDao } from "./dao/CommentDao";
 import { LikeDao } from "./dao/LikeDao";
 import { PostDao } from "./dao/PostDao";
-import { InMemorydatastore } from "./memorydb/index";
+import { SqlDatastore } from "./sql";
+//import { InMemorydatastore } from "./memorydb/index";
 export interface Datastore extends UserDao, PostDao, CommentDao, LikeDao {}
 
-export const db = new InMemorydatastore();
+export let db: Datastore;
+
+export async function initDb() {
+  //db = new InMemorydatastore();
+  db = await new SqlDatastore().openDb();
+}
--- server/index.ts
+++ server/index.ts
@@ -1,32 +1,37 @@
 import express, { ErrorRequestHandler, RequestHandler } from "express";
 import { createPostHandler, listPostHandler } from "./handlers/postHandler";
 import asyncHandler from "express-async-handler";
-const app = express();
+import { initDb } from "./datastore";
 
-const requestLoggerMiddleware: RequestHandler = (req, res, next) => {
-  if (req.method === "POST") {
-    console.log(req.method, req.path, "- body", req.body);
-  } else {
-    console.log(req.method, req.path);
-  }
-  next();
-};
-const errHandler: ErrorRequestHandler = (err, req, res, next) => {
-  return res
-    .sendStatus(500)
-    .send(`Oops , an unexpected error occured, plese try again!`);
-};
+(async () => {
+  await initDb();
+  const app = express();
 
-// Middleware
-app.use(express.json());
-app.use(requestLoggerMiddleware);
-app.use(errHandler);
+  const requestLoggerMiddleware: RequestHandler = (req, res, next) => {
+    if (req.method === "POST") {
+      console.log(req.method, req.path, "- body", req.body);
+    } else {
+      console.log(req.method, req.path);
+    }
+    next();
+  };
+  const errHandler: ErrorRequestHandler = (err, req, res, next) => {
+    return res
+      .sendStatus(500)
+      .send(`Oops , an unexpected error occured, plese try again!`);
+  };
 
-// Routes
-app.get("/posts", listPostHandler);
-app.post("/posts", createPostHandler);
+  // Middleware
+  app.use(express.json());
+  app.use(requestLoggerMiddleware);
+  app.use(errHandler);
 
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`server is running on port ${PORT}`);
-});
+  // Routes
+  app.get("/posts", listPostHandler);
+  app.post("/posts", createPostHandler);
+
+  const PORT = 3000;
+  app.listen(PORT, () => {
+    console.log(`server is running on port ${PORT}`);
+  });
+})();
